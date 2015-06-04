197d7fe069593548ea47a87846e9331c2c55e91c
Mike Hopper <drmikeh@gmail.com>
2015-06-04 10:52:45 -0400

    Fixed SignUp Page.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,5 @@
 class UsersController < ApplicationController
-  before_action :signed_in_user
+  before_action :signed_in_user, only: [:index, :show, :edit, :update, :destroy]
   before_action :correct_user, only: [:show, :edit, :update, :destroy]
   before_action :set_user, only: [:show, :edit, :update, :destroy]
 
